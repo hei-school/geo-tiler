ac69d5b742d578b171c5f31569693f7df7136580
dinasoa <hei.dinasoa@gmail.com>
2023-12-14 15:23:05 +0300

    poja: update poja to 7.0.0

--- src/main/java/school/hei/geotiler/repository/conf/DatasourceConf.java
+++ src/main/java/school/hei/geotiler/repository/conf/DatasourceConf.java
@@ -1,8 +1,7 @@
-package school.hei.geotiler.repository;
+package school.hei.geotiler.repository.conf;
 
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,24 +12,28 @@ import school.hei.geotiler.PojaGenerated;
 @Configuration
 public class DatasourceConf {
 
+  private final String driverClassName;
   private final String appPropDbUrl;
   private final String appProdDbUsername;
   private final String appPropDbPassword;
 
   public DatasourceConf(
+      @Value("${driverClassName:#{null}}") String driverClassName,
       @Value("${spring.datasource.url:#{null}}") String appPropDbUrl,
       @Value("${spring.datasource.username:#{null}}") String appProdDbUsername,
       @Value("${spring.datasource.password:#{null}}") String appPropDbPassword) {
+    this.driverClassName = driverClassName;
     this.appPropDbUrl = appPropDbUrl;
     this.appProdDbUsername = appProdDbUsername;
     this.appPropDbPassword = appPropDbPassword;
   }
 
-  @ConfigurationProperties(prefix = "datasource.postgres")
   @Bean
   @Primary
   public DataSource dataSource() {
     return DataSourceBuilder.create()
+        .driverClassName(
+            driverClassName == null ? System.getenv("DRIVERCLASSNAME") : driverClassName)
         .url(appPropDbUrl == null ? System.getenv("DATABASE_URL") : appPropDbUrl)
         .username(
             appProdDbUsername == null ? System.getenv("DATABASE_USERNAME") : appProdDbUsername)
--- src/test/java/school/hei/geotiler/endpoint/rest/controller/HealthControllerIT.java
+++ src/test/java/school/hei/geotiler/endpoint/rest/controller/HealthControllerIT.java
@@ -1,26 +1,29 @@
 package school.hei.geotiler.endpoint.rest.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static school.hei.geotiler.endpoint.rest.controller.HealthController.OK;
+import static school.hei.geotiler.endpoint.rest.controller.health.PingController.OK;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import school.hei.geotiler.PojaGenerated;
 import school.hei.geotiler.conf.FacadeIT;
+import school.hei.geotiler.endpoint.rest.controller.health.HealthDbController;
+import school.hei.geotiler.endpoint.rest.controller.health.PingController;
 
 @PojaGenerated
 class HealthControllerIT extends FacadeIT {
 
-  @Autowired HealthController healthController;
+  @Autowired PingController pingController;
+  @Autowired HealthDbController healthDbController;
 
   @Test
   void ping() {
-    assertEquals("pong", healthController.ping());
+    assertEquals("pong", pingController.ping());
   }
 
   @Test
   void can_read_from_dummy_table() {
-    var responseEntity = healthController.dummyTable_should_not_be_empty();
+    var responseEntity = healthDbController.dummyTable_should_not_be_empty();
     assertEquals(OK, responseEntity);
   }
 }
