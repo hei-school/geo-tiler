541348f8a653cca8f73e28d7ff6ce40a30b3ad01
dinasoa <hei.dinasoa@gmail.com>
2023-12-15 11:15:23 +0300

    chore: send email when tasks are finished with success

--- src/main/java/school/hei/geotiler/mail/Email.java
+++ src/main/java/school/hei/geotiler/mail/Email.java
@@ -3,9 +3,11 @@ package school.hei.geotiler.mail;
 import jakarta.mail.internet.InternetAddress;
 import java.io.File;
 import java.util.List;
+import lombok.Builder;
 import school.hei.geotiler.PojaGenerated;
 
 @PojaGenerated
+@Builder
 public record Email(
     InternetAddress to,
     List<InternetAddress> cc,
--- src/main/java/school/hei/geotiler/service/ZoneTilingTaskService.java
+++ src/main/java/school/hei/geotiler/service/ZoneTilingTaskService.java
@@ -1,23 +1,36 @@
 package school.hei.geotiler.service;
 
+import static java.io.File.createTempFile;
 import static java.time.Instant.now;
 import static java.util.UUID.randomUUID;
 import static school.hei.geotiler.repository.model.Status.HealthStatus.SUCCEEDED;
 import static school.hei.geotiler.repository.model.Status.ProgressionStatus.FINISHED;
 
+
+import jakarta.mail.internet.AddressException;
+import jakarta.mail.internet.InternetAddress;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import school.hei.geotiler.mail.Email;
+import school.hei.geotiler.mail.Mailer;
+import school.hei.geotiler.model.exception.ApiException;
 import school.hei.geotiler.model.exception.NotFoundException;
 import school.hei.geotiler.repository.ZoneTilingTaskRepository;
 import school.hei.geotiler.repository.model.TaskStatus;
 import school.hei.geotiler.repository.model.ZoneTilingJob;
 import school.hei.geotiler.repository.model.ZoneTilingTask;
+import school.hei.geotiler.utils.HTMLUtils;
 
 @Service
 @AllArgsConstructor
 public class ZoneTilingTaskService {
   private final ZoneTilingTaskRepository repository;
   private final ZoneTilingJobService zoneTilingJobService;
+  private final Mailer mailer;
+  public static final String EMAIL_OBJECT = "Resultat du traitement de zone";
+  public static final String ZONE_TILING_TEMPLATE_NAME = "zone_tiling";
 
   public ZoneTilingTask update(ZoneTilingTask zoneTilingTask) {
     if (!repository.existsById(zoneTilingTask.getId())) {
@@ -31,7 +44,7 @@ public class ZoneTilingTaskService {
     return update(zoneTilingTask);
   }
 
-  public ZoneTilingTask finishWithSuccess(ZoneTilingTask zoneTilingTask) {
+  public void finishWithSuccess(ZoneTilingTask zoneTilingTask) {
     ZoneTilingTask finalized =
         updateStatus(
             zoneTilingTask,
@@ -44,8 +57,22 @@ public class ZoneTilingTaskService {
                 .build());
     ZoneTilingJob associatedJob = zoneTilingJobService.findById(zoneTilingTask.getJobId());
     if (associatedJob.isDone()) {
+      Context context = new Context();
+      context.setVariable("zone", associatedJob.getZoneName());
       zoneTilingJobService.finishWithSuccess(associatedJob);
+      String emailBody = HTMLUtils.parseToString(ZONE_TILING_TEMPLATE_NAME,context);
+      try {
+        mailer.accept(
+            new Email(
+                new InternetAddress(associatedJob.getEmailReceiver()),
+                List.of(),
+                List.of(),
+                EMAIL_OBJECT,
+                emailBody,
+                List.of()));
+      } catch (AddressException e) {
+        throw new ApiException(ApiException.ExceptionType.SERVER_EXCEPTION, e);
+      }
     }
-    return finalized;
   }
 }
