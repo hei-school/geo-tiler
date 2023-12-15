9b5f41d773987546d5c40835e8d90e5cfd60ed1b
Lou Andria <lou@hei.school>
2023-12-15 09:49:22 +0100

    fixup

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 ```
 % cd src/test/resources/rhone
-% wms-tile-get -s lyon_wms.json \
+% wms-tile-get -s grand-lyon-wms.json \
     -g departement-69-rhone-1-sur-400-sur-20-polygones.geojson \
     -z 20 \
-    -o lyon
+    -o grand-lyon-1
 Total: 850, Ok: 850, Failed: 0, Skipped: 0 <-- TODO: check that failed and skipped
 Finished in 6 mn <-- TODO: too borderline as still need to upload to S3 ==> use 1/400/40 instead of 1/400/20
 TODO: variabilize frontal and worker timeout in POJA
+```
+
+```
+% wms-tile-get -s grand-lyon-wms.json \
+               -g departement-69-rhone-grand-lyon-1-sur-1000-polygones.geojson \ 
+               -z 20 \
+               -o grand-lyon-2
+Total: 883, Ok: 883, Failed: 0, Skipped: 0
+Finished in 6 minutes
 ```
--- src/test/java/school/hei/geotiler/repository/model/geo/ParcelIT.java
+++ src/test/java/school/hei/geotiler/repository/model/geo/ParcelIT.java
@@ -1,7 +1,12 @@
 package school.hei.geotiler.repository.model.geo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static school.hei.geotiler.endpoint.rest.model.MultiPolygon.TypeEnum.MULTIPOLYGON;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.net.MalformedURLException;
+import java.net.URL;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import school.hei.geotiler.conf.FacadeIT;
@@ -9,24 +14,18 @@ import school.hei.geotiler.endpoint.rest.model.Feature;
 import school.hei.geotiler.endpoint.rest.model.GeoServerParameter;
 import school.hei.geotiler.endpoint.rest.model.MultiPolygon;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static school.hei.geotiler.endpoint.rest.model.MultiPolygon.TypeEnum.MULTIPOLYGON;
-
 class ParcelIT extends FacadeIT {
 
-  @Autowired
-  ObjectMapper om;
+  @Autowired ObjectMapper om;
 
   @Test
   void serialize_then_deserialize() throws JsonProcessingException, MalformedURLException {
-    var parcel = Parcel.builder()
-        .geoServerUrl(new URL("https://nowhere.com"))
-        .feature(new Feature().geometry(new MultiPolygon().type(MULTIPOLYGON)))
-        .geoServerParameter(new GeoServerParameter().height(1024))
-        .build();
+    var parcel =
+        Parcel.builder()
+            .geoServerUrl(new URL("https://nowhere.com"))
+            .feature(new Feature().geometry(new MultiPolygon().type(MULTIPOLYGON)))
+            .geoServerParameter(new GeoServerParameter().height(1024))
+            .build();
 
     var serialized = om.writeValueAsString(parcel);
     var deserialized = om.readValue(serialized, Parcel.class);
