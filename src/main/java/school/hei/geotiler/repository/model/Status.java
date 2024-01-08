c2e22ed6b5ae5d08fdde87b94a6cd8d338dec42f
Lou Andria <lou@hei.school>
2024-01-08 23:32:41 +0100

    wip= TaskStatus::reduce

--- src/main/java/school/hei/geotiler/repository/model/Status.java
+++ src/main/java/school/hei/geotiler/repository/model/Status.java
@@ -1,5 +1,7 @@
 package school.hei.geotiler.repository.model;
 
+import static java.util.Comparator.comparing;
+import static java.util.Comparator.naturalOrder;
 import static javax.persistence.GenerationType.IDENTITY;
 import static school.hei.geotiler.repository.model.types.PostgresEnumType.PGSQL_ENUM_NAME;
 
@@ -44,7 +46,50 @@ public class Status {
   private String message;
 
   public static Status reduce(List<TaskStatus> statuses) {
-    return null; // TODO
+    var sortedStatuses =
+        statuses.stream()
+            .sorted(comparing(TaskStatus::getCreationDatetime, naturalOrder()))
+            .toList();
+    return sortedStatuses.stream().reduce(sortedStatuses.get(0), Status::reduce);
+  }
+
+  private static TaskStatus reduce(TaskStatus oldStatus, TaskStatus newStatus) {
+    var errorMessage =
+        String.format("Illegal status transition: old=%s, new=%s", oldStatus, newStatus);
+
+    var oldProgression = oldStatus.getProgression();
+    var newProgression = newStatus.getProgression();
+    ProgressionStatus reducedProgression =
+        switch (oldProgression) {
+          case PENDING -> newStatus.getProgression();
+          case PROCESSING -> switch (newProgression) {
+            case PENDING -> throw new IllegalArgumentException(errorMessage);
+            case PROCESSING, FINISHED -> newProgression;
+          };
+          case FINISHED -> switch (newProgression) {
+            case PENDING, PROCESSING -> throw new IllegalArgumentException(errorMessage);
+            case FINISHED -> newProgression;
+          };
+        };
+
+    var oldHealth = oldStatus.getHealth();
+    var newHealth = newStatus.getHealth();
+    HealthStatus reducedHealth =
+        switch (oldHealth) {
+          case UNKNOWN -> newHealth;
+          case SUCCEEDED -> switch (newHealth) {
+            case SUCCEEDED -> newHealth;
+            case UNKNOWN, FAILED -> throw new IllegalArgumentException(errorMessage);
+          };
+          case FAILED -> switch (newHealth) {
+            case FAILED -> newHealth;
+            case UNKNOWN, SUCCEEDED -> throw new IllegalArgumentException(errorMessage);
+          };
+        };
+
+    return oldProgression.equals(reducedProgression) && oldHealth.equals(reducedHealth)
+        ? oldStatus
+        : newStatus;
   }
 
   public enum ProgressionStatus {
