747353db080a895c99a0e52b0c91fc85813b105e
Lou Andria <lou@hei.school>
2024-01-08 23:55:10 +0100

    wip: following de0249cde4df

--- src/main/java/school/hei/geotiler/repository/model/ZoneTilingJob.java
+++ src/main/java/school/hei/geotiler/repository/model/ZoneTilingJob.java
@@ -1,105 +1,53 @@
 package school.hei.geotiler.repository.model;
 
-import static java.util.Comparator.comparing;
-import static school.hei.geotiler.repository.model.Status.HealthStatus.SUCCEEDED;
-import static school.hei.geotiler.repository.model.Status.ProgressionStatus.FINISHED;
+import static java.util.stream.Collectors.toList;
+import static javax.persistence.CascadeType.ALL;
+import static javax.persistence.FetchType.EAGER;
+import static org.hibernate.annotations.FetchMode.SELECT;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.io.Serializable;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
 
 @Entity
 @Getter
+@AllArgsConstructor
+@Builder
 @NoArgsConstructor
 @ToString
 @JsonIgnoreProperties({"status", "done"})
-public class ZoneTilingJob extends ProgressiveAction<JobStatus> {
+public class ZoneTilingJob implements Serializable {
   @Id private String id;
   private String zoneName;
   private String emailReceiver;
   @CreationTimestamp private Instant submissionInstant;
 
-  @OneToMany(cascade = CascadeType.ALL, mappedBy = "jobId", fetch = FetchType.EAGER)
-  @Fetch(FetchMode.SELECT)
+  @OneToMany(cascade = ALL, mappedBy = "jobId", fetch = EAGER)
+  @Fetch(SELECT)
   private List<JobStatus> statusHistory;
 
-  @OneToMany(mappedBy = "jobId", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-  @Fetch(FetchMode.SELECT)
-  private List<ZoneTilingTask> tasks;
+  @OneToMany(mappedBy = "jobId", cascade = ALL, fetch = EAGER)
+  @Fetch(SELECT)
+  private List<ZoneTilingTask> tasks = new ArrayList<>();
 
-  @Override
-  protected List<JobStatus> checkStatusHistory(List<JobStatus> statusHistory) {
-    var sortedStatusHistory = new ArrayList<>(statusHistory);
-    sortedStatusHistory.sort(comparing(Status::getProgression));
-    sortedStatusHistory.stream()
-        .reduce(
-            (s1, s2) ->
-                JobStatus.builder()
-                    .jobId(this.id)
-                    .health(checkHealthTransition(s1.getHealth(), s2.getHealth()))
-                    .progression(
-                        checkProgressionTransition(s1.getProgression(), s2.getProgression()))
-                    .creationDatetime(s2.getCreationDatetime())
-                    .build());
-    return statusHistory;
-  }
-
-  @Override
   public JobStatus getStatus() {
-    List<JobStatus> sortedStatusHistory = new ArrayList<>(statusHistory);
-    sortedStatusHistory.sort(comparing(Status::getCreationDatetime).reversed());
-    return sortedStatusHistory.get(0);
+    return JobStatus.from(
+        id, Status.reduce(statusHistory.stream().map(status -> (Status) status).collect(toList())));
   }
 
-  @Override
   public void addStatus(JobStatus status) {
-    checkStatusTransition(this.getStatus(), status);
-    this.statusHistory.add(status);
-  }
-
-  @Builder
-  public ZoneTilingJob(
-      String id,
-      String zoneName,
-      String emailReceiver,
-      Instant submissionInstant,
-      List<JobStatus> statusHistory,
-      List<ZoneTilingTask> tasks) {
-    this.id = id;
-    this.zoneName = zoneName;
-    this.emailReceiver = emailReceiver;
-    this.submissionInstant = submissionInstant;
-    this.statusHistory = new ArrayList<>(checkStatusHistory(statusHistory));
-    this.tasks = tasks;
-  }
-
-  public boolean succeeded() {
-    return this.getTasks().stream()
-        .allMatch(
-            task -> {
-              TaskStatus status = task.getStatus();
-              return FINISHED.equals(status.getProgression())
-                  && SUCCEEDED.equals(status.getHealth());
-            });
-  }
-
-  public List<ZoneTilingTask> getTasks() {
-    if (tasks == null) {
-      return List.of();
-    }
-    return tasks;
+    statusHistory.add(status);
   }
 }
--- src/main/java/school/hei/geotiler/repository/model/ZoneTilingTask.java
+++ src/main/java/school/hei/geotiler/repository/model/ZoneTilingTask.java
@@ -1,6 +1,6 @@
 package school.hei.geotiler.repository.model;
 
-import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
 import static school.hei.geotiler.repository.model.types.PostgresTypes.JSONB;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -15,6 +15,7 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,72 +28,32 @@ import school.hei.geotiler.repository.model.geo.Parcel;
 
 @TypeDef(name = JSONB, typeClass = JsonBinaryType.class)
 @Entity
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
 @JsonIgnoreProperties({"status"})
-public class ZoneTilingTask extends ProgressiveAction<TaskStatus> implements Serializable {
+public class ZoneTilingTask implements Serializable {
   @Id private String id;
 
   private String jobId;
   @Getter @CreationTimestamp private Instant submissionInstant;
 
   @OneToMany(cascade = CascadeType.ALL, mappedBy = "taskId", fetch = FetchType.EAGER)
-  private List<TaskStatus> statusHistory;
+  private List<TaskStatus> statusHistory = new ArrayList<>();
 
   @Type(type = JSONB)
   @Column(columnDefinition = JSONB)
   private Parcel parcel;
 
-  @Override
-  protected List<TaskStatus> checkStatusHistory(List<TaskStatus> statusHistory) {
-    var sortedStatusHistory = new ArrayList<>(statusHistory);
-    sortedStatusHistory.sort(comparing(Status::getProgression));
-    sortedStatusHistory.stream()
-        .reduce(
-            (s1, s2) ->
-                TaskStatus.builder()
-                    .taskId(this.id)
-                    .health(checkHealthTransition(s1.getHealth(), s2.getHealth()))
-                    .progression(
-                        checkProgressionTransition(s1.getProgression(), s2.getProgression()))
-                    .creationDatetime(s2.getCreationDatetime())
-                    .build());
-    return statusHistory;
-  }
-
-  @Override
   public TaskStatus getStatus() {
-    List<TaskStatus> sortedStatusHistory = new ArrayList<>(statusHistory);
-    sortedStatusHistory.sort(comparing(Status::getCreationDatetime).reversed());
-    return sortedStatusHistory.get(0);
+    return TaskStatus.from(
+        id, Status.reduce(statusHistory.stream().map(status -> (Status) status).collect(toList())));
   }
 
-  @Override
   public void addStatus(TaskStatus status) {
-    checkStatusTransition(this.getStatus(), status);
-    this.statusHistory.add(status);
-  }
-
-  @Builder
-  public ZoneTilingTask(
-      String id,
-      String jobId,
-      Instant submissionInstant,
-      List<TaskStatus> statusHistory,
-      Parcel parcel) {
-    this.id = id;
-    this.jobId = jobId;
-    this.submissionInstant = submissionInstant;
-    this.statusHistory = new ArrayList<>(checkStatusHistory(statusHistory));
-    this.parcel = parcel;
-  }
-
-  public List<TaskStatus> getStatusHistory() {
-    if (statusHistory == null) {
-      return List.of();
-    }
-    return statusHistory;
+    statusHistory.add(status);
   }
 }
